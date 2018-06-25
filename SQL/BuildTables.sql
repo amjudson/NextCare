9cf08697c1f0f73c0a1288ab16ff69dbe3bcd8bf
Mark Judson <mark.judson@eleadcrm.com>
2018-06-25 08:09:02 -0500

    Update to SQL files

--- SQL/BuildTables.sql
+++ SQL/BuildTables.sql
@@ -844,33 +844,6 @@ GO
 ALTER TABLE dbo.StudentInsurance CHECK CONSTRAINT FK_StudentInsurance_Insurance
 GO
 
-/****** Object:  Table dbo.StudentPhone    Script Date: 6/3/2018 2:35:06 PM ******/
-CREATE TABLE dbo.StudentPhone(
-	StudentPhoneId int IDENTITY(1,1) NOT NULL,
-	StudentId int NOT NULL,
-	PhoneId int NOT NULL,
-	IsPrimary bit NOT NULL,
- CONSTRAINT PK_StudentPhone PRIMARY KEY CLUSTERED
-(
-	StudentPhoneId ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE dbo.StudentPhone  WITH CHECK ADD  CONSTRAINT FK_StudentPhone_Student FOREIGN KEY(StudentId)
-REFERENCES dbo.Student (StudentId)
-GO
-
-ALTER TABLE dbo.StudentPhone CHECK CONSTRAINT FK_StudentPhone_Student
-GO
-
-ALTER TABLE dbo.StudentPhone  WITH CHECK ADD  CONSTRAINT FK_StudentPhone_Phone FOREIGN KEY(PhoneId)
-REFERENCES dbo.Phone (PhoneId)
-GO
-
-ALTER TABLE dbo.StudentPhone CHECK CONSTRAINT FK_StudentPhone_Phone
-GO
-
 /****** Object:  Table dbo.StudentPhoto    Script Date: 6/1/2018 8:40:57 AM ******/
 CREATE TABLE dbo.StudentPhoto
 (
