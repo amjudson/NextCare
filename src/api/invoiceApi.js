5e76ce8d3d4142d6e6e8c8b85391822ccf88a960
Mark Judson <mark.judson@eleadcrm.com>
2018-07-06 07:28:22 -0500

    added more APIs

---
+++ src/api/invoiceApi.js
@@ -0,0 +1,46 @@
+import {results} from './fetchApi';
+
+class invoiceApi {
+  static getAllInvoices() {
+    return results(`${process.env.API_HOST}/api/TotalInvoice`, 'GET', 'Invoice').then(invoices => {
+      return new Promise((resolve, reject) => {
+        resolve(Object.assign([], invoices));
+      });
+    });
+  }
+
+  static getInvoiceById(invoiceId) {
+    return results(`${process.env.API_HOST}/api/TotalInvoice/${invoiceId}`, 'GET', 'Invoice')
+      .then(invoice => {
+        return new Promise((resolve, reject) => {
+          resolve(invoice);
+        });
+      });
+  }
+
+  static saveInvoice(invoice) {
+    invoice = Object.assign({}, invoice); // to avoid manipulating object passed in.
+    return new Promise((resolve, reject) => {
+      const mininvoiceNameLength = 2;
+      if (invoice.invoiceId) {
+        results(`${process.env.API_HOST}/api/TotalInvoice/${invoice.invoiceId}`, 'PUT', 'Invoice', invoice)
+          .then((invoice) => {
+            resolve(invoice);
+          });
+      } else {
+        results(`${process.env.API_HOST}/api/Invoice`, 'POST', 'Invoice', invoice).then((invoice) => {
+          resolve(invoice);
+        });
+      }
+    });
+  }
+
+  static deleteInvoice(invoice) {
+    return new Promise((resolve, reject) => {
+      results(`${process.env.API_HOST}/api/TotalInvoice/${invoice.invoiceId}`, 'DELETE', 'State', invoice);
+      resolve();
+    });
+  }
+}
+
+export default invoiceApi;
